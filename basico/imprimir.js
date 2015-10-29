86ad3222181a6d1956cfeb1241c6438d9ca7e70b
Rodolfo Campos <camposer@gmail.com>
2015-10-29 12:45:01 +0100

    Ejemplo básico de función javascript!

---
+++ basico/imprimir.js
@@ -0,0 +1,8 @@
+var imprimir = function(cursor) { 
+	while (cursor.hasNext()) {
+		var p = cursor.next();
+		print("nombre = " + p.nombre); 
+		for (var i in p.ordenadores)
+			print("ordenador.nombre = " + p.ordenadores[i].nombre); 
+	}
+}
